e7f8bf3476e721570e3b59172b555d8ee84667fe
Mark Zimmerman <mark.d.zimmerman@gmail.com>
2026-07-08 10:01:19 -0500

    Add firmware serial heartbeat

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,6 +2,17 @@
 #include "fw_config.h"
 #include "fw_pins.h"
 
+namespace {
+  const char* kBootBanner[] = {
+    "",
+    "FarmWhisper firmware booting...",
+    "Board: Heltec WiFi LoRa 32 V4 (ESP32-S3 class, assumed esp32-s3-devkitc-1 target)",
+    "Display support is optional and disabled by default.",
+  };
+
+  unsigned long lastHeartbeatMs = 0;
+}
+
 void setup() {
   delay(500);
   Serial.begin(115200);
@@ -9,12 +20,15 @@ void setup() {
     delay(10);
   }
 
-  Serial.println();
-  Serial.println("FarmWhisper firmware booting...");
-  Serial.println("Board: Heltec WiFi LoRa 32 V4 (ESP32-S3 class, assumed esp32-s3-devkitc-1 target)");
-  Serial.println("Display support is optional and disabled by default.");
+  for (const char* line : kBootBanner) {
+    Serial.println(line);
+  }
 }
 
 void loop() {
-  delay(1000);
+  const unsigned long now = millis();
+  if (now - lastHeartbeatMs >= 1000) {
+    lastHeartbeatMs = now;
+    Serial.println("FarmWhisper heartbeat");
+  }
 }
